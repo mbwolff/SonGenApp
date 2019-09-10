7f67acf329b0624cbbfc7d21b9e08fd83c1bfe1c
Mark Wolff <wolff.mark.b@gmail.com>
2019-09-10 00:20:14 -0400

    Update config.py

--- sgfr/songen/config.py
+++ sgfr/songen/config.py
@@ -10,7 +10,7 @@ import epitran
 import re
 
 secret_key = b'c-\xf0$\xea\x102\n\xac`\xc5\xbc?VG\x13'
-model_file = 'lib/Fievre_model'
+model_file = '../lib/Fievre_model'
 no_phonemes = 2 # determines the richness of the rhyme
 no_verses = 50 # max number of verses presented to user
 tagdir='/usr/local/treetagger/'
